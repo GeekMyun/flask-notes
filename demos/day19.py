0f9be6dfb933270c12dde222ccd7a8eb59ea3128
GeekMyun <3288610969@qq.com>
2026-08-11 00:33:27 +0800

    update

--- demos/day19.py
+++ demos/day19.py
@@ -24,6 +24,11 @@
 - set块赋值，把一大段赋值给变量，{%set value%}{%endset%}，调用{{value}}
 - 全局自定义变量app.context_processor，所有上下文自带该变量
 
+6.全局自定义变量aoo.context_processor
+- 给所有jinja2模板注入变量，任意html模板可用
+- 函数一定要返回字典，不能return返回字符串，返回空字典{}也合法
+- 多个context_processor可以同时写，返回的字典会全部合并进模板上下文
+- 如果和render_template传入同名的参数，render_template传入的值优先级跟高一些
 '''
 from flask import Flask,render_template
 app = Flask(__name__)
@@ -43,6 +48,30 @@ def index1():
     }
     return render_template('day02-1.html',**dic)
 
+# set定义变量
+@app.route('/set')
+def set():
+    return render_template('day02-2.html')
+
+# set块定义变量
+@app.route('/set_block')
+def set_block():
+    return render_template('day02-3.html')
+
+# Flask内置上下文内变量
+# 不需要要在rend_template传入，janjia2模板可直接使用
+from flask import session,g
+app.secret_key = "my-secret"
+@app.route('/')
+def index3():
+    # 给session赋值
+    session['username'] = 'myun'
+    # 给g对象赋值，本次请求有效
+    g.user = '测试用户'
+    return render_template("day02-4.html")
+@app.route('/hello/<name>')
+def hello(name):
+    return f"hello {name}"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=8080)
