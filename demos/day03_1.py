e2d7707299a8b589ace07f167c84851743f0c2db
GeekMyun <3288610969@qq.com>
2026-08-27 22:13:28 +0800

    update

--- demos/day03_1.py
+++ demos/day03_1.py
@@ -21,8 +21,28 @@ def index(name,age):
     }
     return dict
 
+"""
+2.URl查询参数
+- request.args
+- ?a=xx&b=xx格式
+"""
+from flask import request
+@app.route('/demo')
+def index1():
+    name = request.args.get("name")
+    age = request.args.get("age",default=0,type=int)
+    return f"name={name},age={age}"
 
-
+"""
+3.JSON请求体，接口传参
+- request.get_json()
+"""
+@app.route('/api/test',methods=["POST"])
+def index2():
+    data = request.get_json()
+    a=data.get("a")
+    b=data.get("b")
+    return {"res":a+b}
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=8080)
